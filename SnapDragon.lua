f5395ba15731f3652fd2a7f11a4f103dd14067e5
gwnrdt <jomardelacruz1882@gmail.com>
2025-10-18 19:21:56 +0800

    Implement Snapdragon controller and reference functions

--- SnapDragon.lua
+++ SnapDragon.lua
@@ -1,1 +1,25 @@
+local SnapdragonController = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/SnapDragon/SnapdragonController.lua"))()
+local SnapdragonRef = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/SnapDragon/SnapdragonRef.lua"))()
 
+local function createDragController(...)
+	return SnapdragonController.new(...)
+end
+
+local function setDragState(state)
+	return SnapdragonController:SetDragEnabled(state)
+end
+
+local function createRef(gui)
+	return SnapdragonRef.new(gui)
+end
+
+local export
+export = {
+	createDragController = createDragController, 
+	SnapdragonController = SnapdragonController,
+	setDragState = setDragState,
+	createRef = createRef
+}
+-- roblox-ts `default` support
+export.default = export
+return export
