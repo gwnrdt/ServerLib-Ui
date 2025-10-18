31ede2cd3e65ff47ca6cdf351f753d39c8405130
gwnrdt <jomardelacruz1882@gmail.com>
2025-10-18 19:23:29 +0800

    Update SnapDragon.lua

--- SnapDragon.lua
+++ SnapDragon.lua
@@ -1,5 +1,5 @@
 local SnapdragonController = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/SnapDragon/SnapdragonController.lua"))()
-local SnapdragonRef = loadstring(game:HttpGet("https://raw.githubusercontent.com/tntmaster1385/ServerLib/refs/heads/main/Library/SnapDragon/SnapdragonRef.lua"))()
+local SnapdragonRef = loadstring(game:HttpGet("https://raw.githubusercontent.com/gwnrdt/ServerLib-Ui/refs/heads/main/SnapdragonRef.lua"))()
 
 local function createDragController(...)
 	return SnapdragonController.new(...)
